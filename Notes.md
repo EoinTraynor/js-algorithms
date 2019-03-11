9281141c5867a0d107435eff61c64d212c6ba38d
Eoin Traynor <eointraynor1@gmail.com>
2019-03-11 22:27:59 +0000

    Fib memoized solution

---
+++ Notes.md
@@ -0,0 +1,5 @@
+# Notes
+
+## Memoization
+Store the arguments and result of each function call
+__If the function is called again with the same arguments, return the pre-computed result, so the function does not have to run again__
--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,15 +8,34 @@
 // Example:
 //   fib(4) === 3
 
-// iterative solution 
-function fib(n) {
-  const fib = [0, 1];
-  for (let i = 2; i <= n; i++) {
-    const previousNumber = fib[i-1];
-    const secondPreviousNumber = fib[i-2];
-    fib.push(previousNumber + secondPreviousNumber);
-  }
-  return fib[n];
+// Memoize
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) return cache[args];
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
 }
+
+// Recursive Solution
+// Time complexity: Exponential Runtime
+function fib(n) {
+  if (n < 2) return n;
+  return fib(n-1) + fib(n-2);
+}
+
+fib = memoize(fib);
+// Iterative Solution 
+// function fib(n) {
+//   const fib = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     const previousNumber = fib[i-1];
+//     const secondPreviousNumber = fib[i-2];
+//     fib.push(previousNumber + secondPreviousNumber);
+//   }
+//   return fib[n];
+// }
 
 module.exports = fib;
