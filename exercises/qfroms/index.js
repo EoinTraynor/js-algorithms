a0ac31ece0ff013cf1f0aa20a74cfd09c96dcf0a
Eoin Traynor <eointraynor1@gmail.com>
2019-03-14 21:18:46 +0000

    Emulate a queue with two stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,34 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stackA = new Stack;
+    this.stackB = new Stack;
+  }
+  add(n) {
+    this.stackA.push(n);
+  }
+  remove() {
+    while (this.stackA.peek()) {
+      this.stackB.push(this.stackA.pop());
+    }
+    const poppedStack = this.stackB.pop();
+    while (this.stackB.peek()) {
+     this.stackA.push(this.stackB.pop()); 
+    }
+    return poppedStack;
+  }
+  peek() {
+    while (this.stackA.peek()) {
+      this.stackB.push(this.stackA.pop());
+    }
+    const lastQueueValue = this.stackB.peek();
+    while (this.stackB.peek()) {
+      this.stackA.push(this.stackB.pop());
+    }
+    return lastQueueValue;
+  } 
+}
 
 module.exports = Queue;
